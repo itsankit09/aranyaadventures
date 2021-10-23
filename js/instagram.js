969e0b71034a7fb51de742a68c42121788ee06dc
ankit.vishnoi <ankit.vishnoi@servicenow.com>
2021-10-24 01:51:51 +0530

    Updating site

---
+++ js/instagram.js
@@ -0,0 +1,75 @@
+
+var instagramPosts = [];
+var fetchUrl = 'https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=IGQVJXb2RtR0tpZATNmb0l2T2ZAlbk5RNENCM1BfNkg0TDg4dGQ1X28xcmFBeTRiVnh4dWVhd2hveXNtU3pqSjhUVjRSUEZAsd1V2OUZARaEY2enpMdWg4SFBxVlpRNlUwNGxESDBQNFBrVjFuVnU3Q0xJYwZDZD'
+var postsCount = 0;
+var currentDisplayPostcount = 0;
+
+function populatePosts(posts){
+    const instaGrid = document.getElementsByClassName("instagram-grid")[0];
+    let  child = '';
+    const len = posts.length;
+    var maxPost = currentDisplayPostcount;
+    posts.forEach((post, i)=> {
+        try{
+            if(i<maxPost){
+                if(post.media_type === 'VIDEO'){
+                    // child += createNewVideoPost(post.media_url, post.caption, post.permalink)
+                    maxPost++;
+                }else{
+                    child += createNewPost(post.media_url, post.caption, post.permalink)
+                }
+            }
+        }catch(e){
+
+        }
+    })
+    instaGrid.innerHTML = child;
+}
+
+function createNewPost(image, content, hyperlink) {
+    return `
+        <div class="element-item post">
+            <a class="popup-with-move-anim" href="${hyperlink}">
+                <div class="element-item-overlay">
+                    <span><div>${content.normalize()}</div></span>
+                </div>
+                <img src="${image}" alt="alternative">
+            </a>
+        </div>
+    `
+}
+
+function createNewVideoPost(image, content, hyperlink) {
+    return `
+        <div class="element-item post">
+            <a class="popup-with-move-anim" >
+                <div class="element-item-overlay">
+                    <span><div>${content.normalize()}</div></span>
+                </div>
+                <video width="480" height="480" controls autoplay>
+                    <source src="${image}" type="video/mp4"/>
+                </video>
+            </a>
+        </div>
+    `
+}
+
+function fetchNewPosts(){
+    fetch(fetchUrl)
+    .then(response => response.json())
+    .then(data => {
+        instagramPosts = [...instagramPosts, ...data.data];
+        postsCount = instagramPosts.length;
+        fetchUrl = data.paging.next;
+        populatePosts(instagramPosts);
+    })
+}
+
+function getPosts() {
+    currentDisplayPostcount += 12
+    if(currentDisplayPostcount > postsCount){
+        fetchNewPosts();
+    }else{
+        populatePosts(instagramPosts)
+    }
+}
